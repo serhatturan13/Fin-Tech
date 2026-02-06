a8e60871cc52ca390a2ff284ef8c1bc352446358
serhatturan13 <serhat.turan137@gmail.com>
2026-02-06 23:37:21 +0300

    Yfinance ile altın verileri çekildi

---
+++ data_loader.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import pandas as pd
+
+#hangi veriyi istiyoruz?/(Ex:Altın- GC=F veya Türk Hava Yolları- THYAO.IS)
+symbol = "GC-F"
+# 2. veriyi çekelim
+print(f"{symbol} veriler indiriliyor")
+data = yf.download(symbol, start="2023-01-01", end="2026-01-01")
+# 3. ilk 5 satıra bir bakalım (matematiksel kontrol)
+print(data.head())
+#4. veriyi bir CSV dosyasına kaydedelim ki elimizde somut bir şey olsun
+data.to_csv("altin_verileri.csv")
+print("veri başarıyla 'altin_verileri.csv' olarak kaydedildi")
